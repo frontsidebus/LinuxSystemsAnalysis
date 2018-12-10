c3951ea5e67ed2c04ceec5642981d86b5a94de9c
Paul B <paulebryant@gmail.com>
2018-12-10 12:15:04 -0600

    Update get_sys_analysis_tools.sh

--- get_sys_analysis_tools.sh
+++ get_sys_analysis_tools.sh
@@ -18,7 +18,7 @@ declare -a pkgmgrs=("dnf" "yum" "aptitude")
 
 PKG_MGR=$(for i in "${pkgmgrs[@]}"
 	do
-		which $i 2>/dev/null | sed -r 's/$pkgmgrs/\t/g'
+		which $i 2>/dev/null | cut -d "\/" -f 3
 	done
 )
 
